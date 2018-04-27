b4e5b7180665dd32acf960c8dbc58f44c3d78317
imranmomin <imran.amomin@outlook.com>
2018-04-27 10:04:31 -0400

    updated the queue monitoring methods

--- Hangfire.Couchbase/Queue/Interface/IPersistentJobQueueMonitoringApi.cs
+++ Hangfire.Couchbase/Queue/Interface/IPersistentJobQueueMonitoringApi.cs
@@ -10,6 +10,7 @@ namespace Hangfire.Couchbase.Queue
         IEnumerable<string> GetQueues();
         IEnumerable<string> GetEnqueuedJobIds(string queue, int from, int perPage);
         IEnumerable<string> GetFetchedJobIds(string queue, int from, int perPage);
+        (int? EnqueuedCount, int? FetchedCount) GetEnqueuedAndFetchedCount(string queue);
         int GetEnqueuedCount(string queue);
     }
 }
--- Hangfire.Couchbase/Queue/JobQueueMonitoringApi.cs
+++ Hangfire.Couchbase/Queue/JobQueueMonitoringApi.cs
@@ -58,15 +58,42 @@ namespace Hangfire.Couchbase.Queue
             {
                 BucketContext context = new BucketContext(bucket);
                 return context.Query<Documents.Queue>()
-                    .Where(q => q.DocumentType == DocumentTypes.Queue && q.Name == queue)
+                    .Where(q => q.DocumentType == DocumentTypes.Queue && q.Name == queue && q.FetchedAt.HasValue == false)
                     .OrderBy(q => q.CreatedOn)
-                    .Skip(from).Take(perPage)
+                    .Skip(from + 1).Take(perPage + 1)
                     .Select(c => c.JobId)
                     .AsEnumerable();
             }
         }
 
-        public IEnumerable<string> GetFetchedJobIds(string queue, int from, int perPage) => GetEnqueuedJobIds(queue, from, perPage);
+        public IEnumerable<string> GetFetchedJobIds(string queue, int from, int perPage)
+        {
+            using (IBucket bucket = storage.Client.OpenBucket(storage.Options.DefaultBucket))
+            {
+                BucketContext context = new BucketContext(bucket);
+                return context.Query<Documents.Queue>()
+                    .Where(q => q.DocumentType == DocumentTypes.Queue && q.Name == queue && q.FetchedAt.HasValue)
+                    .OrderBy(q => q.CreatedOn)
+                    .Skip(from + 1).Take(perPage + 1)
+                    .Select(c => c.JobId)
+                    .AsEnumerable();
+            }
+        }
 
+        public (int? EnqueuedCount, int? FetchedCount) GetEnqueuedAndFetchedCount(string queue)
+        {
+            using (IBucket bucket = storage.Client.OpenBucket(storage.Options.DefaultBucket))
+            {
+                BucketContext context = new BucketContext(bucket);
+                (int Fetched, int Enqueued) result = context.Query<Documents.Queue>()
+                    .Where(q => q.DocumentType == DocumentTypes.Queue && q.Name == queue)
+                    .AsEnumerable()
+                    .GroupBy(q => q.Name)
+                    .Select(v => (Fetched: v.Sum(q => q.FetchedAt.HasValue ? 1 : 0), Enqueued: v.Sum(q => q.FetchedAt.HasValue ? 0 : 1)))
+                    .FirstOrDefault();
+
+                return result;
+            }
+        }
     }
 }
