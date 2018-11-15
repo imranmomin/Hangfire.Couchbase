38a5f697d9f1f99b827a04c96b7a3c148ed731a1
imranmomin <imran.amomin@outlook.com>
2018-11-15 10:51:41 -0500

    couchbase rabbitmq project

---
+++ Hangfire.Couchbase.RabbitMq/RabbitMqCouchbaseStorageExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Hangfire.Couchbase.RabbitMq
+{
+    public static class RabbitMqCouchbaseStorageExtension
+    {
+        public static CouchbaseStorage UseRabbitMq(this CouchbaseStorage storage, params string[] queues)
+        {
+            return storage;
+        }
+    }
+}
---
+++ Hangfire.Couchbase.RabbitMq/RabbitMqFetchedJob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Hangfire.Storage;
+
+namespace Hangfire.Couchbase.RabbitMq
+{
+    internal class RabbitMqFetchedJob : IFetchedJob
+    {
+        public string JobId { get; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFromQueue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Requeue()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
