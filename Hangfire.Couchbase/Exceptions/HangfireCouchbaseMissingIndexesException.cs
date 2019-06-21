91030804dee858f91e4ffd88aa64f1c5be43f86d
imranmomin <imran.amomin@outlook.com>
2019-06-21 09:50:34 -0400

    moved the exception files

---
+++ Hangfire.Couchbase/Exceptions/HangfireCouchbaseMissingIndexesException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hangfire.Couchbase.Exceptions
+{
+    internal class HangfireCouchbaseMissingIndexesException : System.Exception
+    {
+        /// <summary>Initializes a new instance of the <see cref="T:Hangfire.Couchbase.Exceptions.HangfireCouchbaseMissingIndexesException" /> class with a specific message that describes the current exception.</summary>
+        /// <param name="message">A message that describes the current exception.</param>
+        public HangfireCouchbaseMissingIndexesException(string message) : base(message, null)
+        {
+        }
+    }
+}
